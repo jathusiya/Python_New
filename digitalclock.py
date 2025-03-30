f39478d4c332a7ca102de9c589b9bba24aeb1623
unknown <jathuseelan96@gmail.com>
2025-03-30 15:30:53 +0100

    updated

---
+++ digitalclock.py
@@ -0,0 +1,7 @@
+from tkinter import*
+clk = Tk()
+clk.title("clock")
+clk.geometry("1350x700+0+0")
+clk.config(bg="blue")
+
+ib_hr = Label(clk,text ="12", font=("times 20 bold", 75, 'bold'), bg = 'yellow', fg = )
