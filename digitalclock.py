d094227476dbcc958b69a8773a06a4c5f3e2082d
unknown <jathuseelan96@gmail.com>
2025-03-31 17:55:13 +0100

    digital updated

--- digitalclock.py
+++ digitalclock.py
@@ -4,4 +4,16 @@ clk.title("clock")
 clk.geometry("1350x700+0+0")
 clk.config(bg="blue")
 
-ib_hr = Label(clk,text ="12", font=("times 20 bold", 75, 'bold'), bg = 'yellow', fg = )
+ib_hr = Label(clk,text ="12", font=("times 20 bold", 75, 'bold'), bg = 'yellow', fg = "white")
+ib_hr.place(x=350, y=200, width=150, height=150)
+
+ib_hr_txt = Label(clk, text= "HOUR", font=("Times 20 bold", 20, "bold"),bg = "#87587", fg="white")
+ib_hr_txt.place(x=350, y= 360, width = 150, height= 50)
+
+ib_mn = Label(clk,text ="12", font=("times 20 bold", 75, 'bold'), bg = 'yellow', fg = "white")
+ib_mn.place(x=350, y=200, width=150, height=150)
+
+ib_mn_txt = Label(clk, text= "HOUR", font=("Times 20 bold", 20, "bold"),bg = "#87587", fg="white")
+ib_mn_txt.place(x=350, y= 360, width = 150, height= 50)
+
+clk.mainloop()
